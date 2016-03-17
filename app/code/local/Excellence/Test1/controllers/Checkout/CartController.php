419488be6cef149da265784abcc771d382740f98
satyametch <satya@excellencetechnologies.in>
2016-03-17 15:34:57 +0530

    Password_Validation

--- app/code/local/Excellence/Test1/controllers/Checkout/CartController.php
+++ app/code/local/Excellence/Test1/controllers/Checkout/CartController.php
@@ -3,7 +3,7 @@ require_once 'Mage/Checkout/controllers/CartController.php';
 class Excellence_Test1_Checkout_CartController extends Mage_Checkout_CartController{
 
 	public function indexAction(){
-		//echo "hello";
+		echo "hello";
 	}
 
 }
