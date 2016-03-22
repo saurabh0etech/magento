857941df4e3ceddc702294daf2c0eec2d66890be
satyametch <satya@excellencetechnologies.in>
2016-03-22 14:54:15 +0530

    Events-Observers

--- app/code/local/Excellence/Phone/Model/Observer.php
+++ app/code/local/Excellence/Phone/Model/Observer.php
@@ -1,6 +1,6 @@
 <?php
 class Excellence_Phone_Model_Observer {
-    
+
     public function loginStatus(Varien_Event_Observer $observer) {   
         Mage::log("hello"); 
         $event = $observer->getEvent();       
@@ -12,9 +12,9 @@ class Excellence_Phone_Model_Observer {
         //echo $model; die();
         Mage::getSingleton('core/session')->addSuccess('Customer Information saved successfully');        
         $id = $model->save()->getId();
-        $myValue = $id;
+        $value = $id;
         //echo $myValue; die();
-        Mage::getSingleton('core/session')->setMyValue($myValue);        
+        Mage::getSingleton('core/session')->setMyId($value);        
     }
     public function logoutStatus( Varien_Event_Observer $observer) { 
         //Mage::log("hello world");
@@ -22,12 +22,12 @@ class Excellence_Phone_Model_Observer {
         $customer = $event->getCustomer();
         $email = $customer->getEmail();
         $id = $customer->getId();         
-        $myValue = '';
-        $myValue = Mage::getSingleton('core/session')->getMyValue();           
+        $value = '';
+        $value = Mage::getSingleton('core/session')->getMyId();           
         $dateTime = Mage::getModel('core/date')->date('Y-m-d H:i:s');        
         $arr['logout_time'] = $dateTime;
         //print_r($timeArray); die();
-        $model = Mage::getModel('phone/login')->load($myValue);
+        $model = Mage::getModel('phone/login')->load($value);
         $model->addData($arr);
         $model->save();
         Mage::getSingleton('core/session')->addSuccess('Customer Information saved successfully');      
